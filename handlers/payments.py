aea6972b4008c1ef27b9f0bf2dec4f4e24ff90ee
Mikhail Krotov <genry.foer@gmail.com>
2023-09-05 12:41:32 +0600

    update payments

--- handlers/payments.py
+++ handlers/payments.py
@@ -5,21 +5,28 @@ from consts.payment_consts import tokens_by_cost, tokens_prices, PAYMENT_TOKEN
 from consts.admins import admins
 from aiogram import types
 from aiogram.types.message import ContentType
+from typing import Any
+import time
 
-@dp.callback_query_handler(text='120tokens')
+@dp.callback_query_handler(text='110tokens')
 async def buy_120_tokens(callback: types.CallbackQuery):
   await callback.message.delete()
-  await send_invoice(callback.message.chat.id, 120)
+  await send_invoice(callback.message.chat.id, 110)
 
-@dp.callback_query_handler(text='1300tokens')
+@dp.callback_query_handler(text='570tokens')
 async def buy_1300_tokens(callback: types.CallbackQuery):
   await callback.message.delete()
-  await send_invoice(callback.message.chat.id, 1300)
+  await send_invoice(callback.message.chat.id, 570)
 
-@dp.callback_query_handler(text='15000tokens')
+@dp.callback_query_handler(text='1200tokens')
+async def buy_1300_tokens(callback: types.CallbackQuery):
+  await callback.message.delete()
+  await send_invoice(callback.message.chat.id, 1200)
+
+@dp.callback_query_handler(text='13000tokens')
 async def buy_15000_tokens(callback: types.CallbackQuery):
   await callback.message.delete()
-  await send_invoice(callback.message.chat.id, 15000)
+  await send_invoice(callback.message.chat.id, 13000)
 
 @dp.message_handler(commands=['buy'])
 async def donate(message: types.Message):
@@ -36,26 +43,41 @@ async def donate(message: types.Message):
 def create_keyboard():
   keyboard = types.InlineKeyboardMarkup()
   keyboard.add(types.InlineKeyboardButton(
-    text='120 токенов [100 RUB]',
-    callback_data='120tokens')
+    text='110 токенов [100 RUB]',
+    callback_data='110tokens')
   )
   keyboard.add(types.InlineKeyboardButton(
-    text='1300 токенов [1000 RUB]',
-    callback_data='1300tokens')
+    text='570 токенов [500 RUB]',
+    callback_data='570tokens')
   )
   keyboard.add(types.InlineKeyboardButton(
-    text='15000 токенов [10000 RUB]',
-    callback_data='15000tokens')
+    text='1200 токенов [1000 RUB]',
+    callback_data='1200tokens')
+  )
+  keyboard.add(types.InlineKeyboardButton(
+    text='13000 токенов [10000 RUB]',
+    callback_data='13000tokens')
   )
   return keyboard
 
-async def send_success_admin(user_id, username, payment_id, payment):
-  text = 'Успешная транзакция:\n'
-  text += f'\nuser_id: {user_id}'
-  text += f'\nusername: @{username}'
-  text += f'\npayment_id: {payment_id}'
-  text += f'\namount: {str(payment.amount.value)} {str(payment.amount.currency)}'
-  await bot.send_message(admins[0], text)
+def save_payment_info(payment_info: dict[str, Any], user_id: int, username: str):
+  payment_id = None
+  payment_obj = { 'username': username }
+  payment_obj['created_at'] = time.time() * 1000
+  payment_obj['user_id'] = user_id
+  for key, value in payment_info.items():
+    if key == 'provider_payment_charge_id':
+      payment_id = value
+    else:
+      payment_obj[key] = value
+  db_service.set_obj_by_id(PAYMENTS_DB_KEY, payment_id, payment_obj)
+
+async def send_success_admin(payment_info: dict[str, Any], username: str):
+  admin_text = 'Успешная транзакция:\n'
+  for key, value in payment_info.items():
+    admin_text += f'{key}: {value}\n'
+  admin_text += f'username: @{username}'
+  await bot.send_message(admins[0], admin_text)
 
 # pre checkout  (must be answered in 10 seconds)
 @dp.pre_checkout_query_handler(lambda query: True)
@@ -66,23 +88,19 @@ async def pre_checkout_query(pre_checkout_q: types.PreCheckoutQuery):
 @dp.message_handler(content_types=ContentType.SUCCESSFUL_PAYMENT)
 async def successful_payment(message: types.Message):
   purchased_tokens = tokens_by_cost[message.successful_payment.total_amount]
-  user = db_service.get_obj_by_id(USERS_DB_KEY, message.from_user.id)
+  user_id = user_id
+  user = db_service.get_obj_by_id(USERS_DB_KEY, user_id)
   user['tokens'] = user['tokens'] + purchased_tokens
-  db_service.set_obj_by_id(USERS_DB_KEY, message.from_user.id, user)
+  db_service.set_obj_by_id(USERS_DB_KEY, user_id, user)
 
-  await bot.send_message(message.from_user.id,
+  await bot.send_message(user_id,
                         f'Платеж на сумму {message.successful_payment.total_amount // 100} {message.successful_payment.currency} прошел успешно!\nЗачислено {purchased_tokens} токенов\n\n/tokens - Остаток токенов')
 
-  # TODO: save provider_payment_charge_id to payments db
-  # NOTE: send provider_payment_charge_id to user or not?
-
-  # TODO: move to send success admin method
-  admin_text = 'SUCCESSFUL PAYMENT:\n'
   payment_info = message.successful_payment.to_python()
-  for k, v in payment_info.items():
-    admin_text += f'{k}: {v}\n'
-  admin_text += f'username: @{user["username"]}'
-  await bot.send_message(admins[0], admin_text)
+  username = user['username']
+
+  await send_success_admin(payment_info, username)
+  save_payment_info(payment_info, user_id, username)
 
 async def send_invoice(user_id, tokens):
   PRICE = calculate_price(tokens)
