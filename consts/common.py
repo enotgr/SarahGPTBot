468cbbb006c4681715e21307da732cccbf1a83a3
Mikhail Krotov <genry.foer@gmail.com>
2024-04-17 21:27:49 +0400

    update

--- consts/common.py
+++ consts/common.py
@@ -10,5 +10,5 @@ image_cost = 10
 
 gpt_models_map = {
   'gpt3': { 'name': 'gpt-3.5-turbo', 'cost': 1},
-  'gpt4': { 'name': 'gpt-4-turbo', 'cost': 12 },
+  'gpt4': { 'name': 'gpt-4-turbo-preview', 'cost': 12 },
 }
